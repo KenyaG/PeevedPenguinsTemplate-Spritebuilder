7584cc21f6422c49be18b12adfc8333061150fff
Kenya <kegordon@loaner3857.com>
2014-06-27 11:52:00 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/GamePlay.h
@@ -0,0 +1,13 @@
+//
+//  GamePlay.h
+//  PeevedPenguins
+//
+//  Created by Kenya Gordon on 6/27/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface GamePlay : CCNode
+
+@end
